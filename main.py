9d092e36aa7ffd7c8adeb29a591719e5df9395fe
nsinghsingh <nihalsinsin@gmail.com>
2019-11-28 09:53:05 +0000

    11, 13 & 14

--- main.py
+++ main.py
@@ -35,14 +35,41 @@ us_census['Men'] = pandas.to_numeric(us_census['Men'])
 print(us_census.head())
 
 #8
-plt.scatter(us_census['Women'], us_census['Income'])
+plt.scatter(us_census['Women'], us_census['Income']) #Soll nicht funktionieren nach Aufgabe
 plt.show()
+plt.savefig('myhistogram.png')
 
 #9
 us_census = us_census.fillna(value={"Women":us_census['TotalPop'] - us_census['Men']})
 print(us_census.head())
 
 #10
-duplicates = us_census.duplicated()
+duplicates = us_census.duplicated(subset='TotalPop')
 print(duplicates)
 
+#11
+us_census = us_census.drop_duplicates(subset='TotalPop')
+print(us_census)
+
+#12
+plt.scatter(us_census['Women'], us_census['Income']) #Soll funktionieren nach Aufgabe
+plt.show()
+plt.savefig('myhistogram.png')
+
+#13
+print(us_census.columns)
+
+#14
+races = ['Hispanic', 'White', 'Black', 'Native', 'Asian', 'Pacific']
+
+for race in races:
+  us_census[race] = us_census[race].replace('%', '', regex=True)
+  us_census[race] = pandas.to_numeric(us_census[race])
+  us_census = us_census.fillna(value={race:0})
+  plt.hist(us_census[race]) #Soll funktionieren nach Aufgabe
+  plt.show()
+  plt.savefig(race + '_histogramm.png')
+print(us_census.dtypes)
+print(us_census.head())
+
+#15
