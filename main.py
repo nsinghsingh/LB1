8457ab65337cd0e0f611f13c83098eac982ad5dd
nsinghsingh <nihalsinsin@gmail.com>
2019-11-28 08:33:35 +0000

    5, 6 & 7

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ print(us_census.columns)
 #4
 print(us_census.head())
 
+#5
+us_census['Income'] = us_census['Income'].replace('\$', '', regex=True)
+print(us_census.head())
+
+#6
+genders = us_census.GenderPop.str.split('_', expand=True)
+
+
+#7
+genders = genders.replace('(F|M)', '', regex=True)
+genders[0] = pandas.to_numeric(genders[0])
+genders[1] = pandas.to_numeric(genders[1])
